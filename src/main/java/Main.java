701fa65eec1fc10b8544f1a83a486c5986fe3ae2
Samuel Vitor Mariano <169068157+Samuel123450908@users.noreply.github.com>
2024-05-27 12:31:02 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Potência e Consumo");
+    System.out.println("");
+    System.out.println("valor da potência ");
+    double potencia = 200;
+    System.out.println(potencia);
+
+    System.out.println("valor das horas");
+    double horas = 3;
+    System.out.println(horas);
+
+      double dias = 30;
+    System.out.println(dias);
+      
+  }
+}
