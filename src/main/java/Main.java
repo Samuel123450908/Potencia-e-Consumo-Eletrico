ddb96b8759d4610150c8b75a323cf52360c064d4
Samuel Vitor Mariano <169068157+Samuel123450908@users.noreply.github.com>
2024-05-27 14:35:43 +0000

    Update final

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,12 +7,33 @@ public class Main {
     System.out.println("Uma tv de 200 watts fica ligada aproximadamente 5h diariamente. Determine o consumo eletrico mensal da tv");
     System.out.println("");
     System.out.println("valor da potência ");
-    double potencia = 200;
+    int potencia = 200;
     System.out.println(potencia);
 
-    System.out.println("valor das horas");
-    double tempo = 5;
-    System.out.println(tempo);
+    System.out.println("dias");
+    int hora = 5;
+    System.out.println(hora);
+System.out.println("");
+
+    System.out.println("dias do mês ");
+    int mes = 30;
+    System.out.println(mes);
+    System.out.println("");
+    
+    System.out.println("divida valor da potencia, o valor da hora e o valor do mês por mil");
+   
+    System.out.println("");
+    int mil = 1000;
+    System.out.println(mil);
+    
+System.out.println("");
+    
+    int consumo = potencia * hora * mes / mil;
+   
+    System.out.println("");
+    System.out.println("consumo");
+    System.out.println(consumo);
+    
     
 
     
