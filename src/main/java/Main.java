a90b8b77a4dc7e69da0855d50734534dd41d92ec
Samuel Vitor Mariano <169068157+Samuel123450908@users.noreply.github.com>
2024-05-27 12:39:20 +0000

    Update 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,16 +3,20 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Potência e Consumo");
     System.out.println("");
+
+    System.out.println("Uma tv de 200 watts fica ligada aproximadamente 5h diariamente. Determine o consumo eletrico mensal da tv");
+    System.out.println("");
     System.out.println("valor da potência ");
     double potencia = 200;
     System.out.println(potencia);
 
     System.out.println("valor das horas");
-    double horas = 3;
-    System.out.println(horas);
+    double tempo = 5;
+    System.out.println(tempo);
+    
 
-      double dias = 30;
-    System.out.println(dias);
+    
+
       
   }
 }
